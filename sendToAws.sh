5f2582ae6391855cbec8a79f44cdb0623af7290b
David Lozzi <lozzi.david@bcg.com>
2020-04-07 17:02:42 -0400

    small build tweaks

--- sendToAws.sh
+++ sendToAws.sh
@@ -5,7 +5,9 @@ docker build -t virtual-happy-hour .
 docker tag virtual-happy-hour 300420735591.dkr.ecr.us-west-2.amazonaws.com/virtual-happy-hour/nodejs
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 300420735591.dkr.ecr.us-west-2.amazonaws.com
 docker push 300420735591.dkr.ecr.us-west-2.amazonaws.com/virtual-happy-hour/nodejs
-task=$(sed s/\"//g <<<$(aws ecs list-tasks --cluster virtual-happy-hour --output json | jq .taskArns[0]))
+
+# how best to restart the image? stop and restart the task?
+# task=$(sed s/\"//g <<<$(aws ecs list-tasks --cluster virtual-happy-hour --output json | jq .taskArns[0]))
 # need a pause or loop check
-aws ecs run-task --cluster virtual-happy-hour --task $task
+# aws ecs run-task --cluster virtual-happy-hour --task $task
 # need to start
